879aacf6fd29b052a893a00034127646dffaa41b
soumen bhunia <sob99338@gmail.com>
2023-07-29 19:23:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cubikor_getyourbestcube/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cubikor_getyourbestcube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
